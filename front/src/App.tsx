9ca45daf91821c11ba547fec2ec8955bdf10c181
Etienne Crombez <e.crombez@betclicgroup.com>
2021-12-09 12:10:44 +0100

    :heavy_plus_sign: add stories lib

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Stories from 'react-insta-stories';
+import { Story } from 'react-insta-stories/dist/interfaces';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const stories: Story[] = [
+        {
+            url: 'https://www.volleypack.fr/wp-content/uploads/2020/01/interview-volleypack-jean-patry-2020-4.jpg',
+        },
+        {
+            url: 'https://cdn-s-www.dna.fr/images/261B2D20-FE2E-4058-B1FB-28483A2A220D/NW_raw/lucille-gicquel-(a-gauche)-isaline-sager-weider-(n-12)-nina-stojiljkovic-et-les-bleues-defient-la-redoutable-serbie-en-quart-de-finale-de-l-euro-photo-cev-julien-crosnier-1630443976.jpg',
+        },
+    ];
+    return <Stories stories={stories} defaultInterval={1500} loop={true} width={'100vw'} height={'100vh'} />;
 }
 
 export default App;
